e12f5277f63e8d2545a8f9640dbd8e69582c6fef
Tomer Dev <tomer@careback.co>
2021-09-29 02:10:41 +0300

    start date is when user is hired

--- models/user.model.js
+++ models/user.model.js
@@ -1,25 +1,23 @@
 const mongoose = require("mongoose");
 const User = mongoose.model(
   "User",
-  new mongoose.Schema(
-    {
-      firstName: String,
-      lastName: String,
-      email: String,
-      password: String,
-      phone: String,
-      address: String,
-      roll: String,
-      isHired: { type: Boolean, default: false },
-      roles: [
-        {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Role",
-        },
-      ],
-    },
-    { timestamps: true }
-  )
+  new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    phone: String,
+    address: String,
+    roll: String,
+    isHired: { type: Boolean, default: false },
+    startDate: Date,
+    roles: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Role",
+      },
+    ],
+  })
 );
 
 module.exports = User;
